101c552c200e70c0dabd9ef4c624131f2332e315
Bevan Collins <b.collins@sentrybay.com>
2026-02-17 09:46:16 +1300

    don't overwrite widget height with default

--- src/widget.cpp
+++ src/widget.cpp
@@ -3,13 +3,13 @@
 #include "fontmetrics.hpp"
 
 void widget::process_xaml(const pugi::xml_node& xaml) {
+  YGNodeStyleSetHeight(node_, font_metrics::instance().dlu_to_dip_y(default_height_dlu()));
+
   resource::process_xaml(xaml);
 
   auto is_tab_stop = xaml.attribute("IsTabStop").as_bool(true);
   if (is_tab_stop)
     style_.push_back("WS_TABSTOP");
-
-  YGNodeStyleSetHeight(node_, font_metrics::instance().dlu_to_dip_y(default_height_dlu()));
 }
 
 int widget::default_height_dlu() const {
